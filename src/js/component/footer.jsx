99d9005fa873ebb6dc5b94f5a086b9cd510ded00
benjalonso <alonsobenjaminlb@gmail.com>
2022-04-14 19:26:10 +0000

    pagina web con react terminada

---
+++ src/js/component/footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const Footer = () => {
+	return (
+		<>
+			<div className="container-fluid text-white bg-dark p-3">
+				<p className="text-center">
+					Copyright &copy; Your website 2019
+				</p>
+			</div>
+		</>
+	);
+};
